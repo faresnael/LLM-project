5837f8fd05f4547891ffd362452181ecbc20c41c
fares <fareselattar@gmail.com>
2025-04-07 20:28:49 -0700

    implemented embedding layers and dataloader

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class simpleTokenizerv1:
     def encode(self,text):
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)',text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
     
@@ -25,18 +27,23 @@ preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)',raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 
 all_words = sorted(set(preprocessed))
+all_words.extend(["<|endoftext|>","<|unk|>"])
 # for i,j in enumerate(all_words):
 #     print("i is: ", i)
 #     print("j is: ", j)
 vocab = {token:integer for integer,token in enumerate(all_words)}
 tokenizer = simpleTokenizerv1(vocab)
-text = """" It's the last he painted, you know,"
-        Mrs. Gisburn said with pardonable pride."""
+# text = """" It's the last he painted, you know,"
+#         Mrs. Gisburn said with pardonable pride."""
 
 # ids = tokenizer.encode(text)
 # print(ids)
 # print (tokenizer.decode(ids))
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1,text2))
 
-
+print(text)
+print (tokenizer.encode(text))
 
 
---
+++ t1.py
@@ -0,0 +1,57 @@
+import tiktoken
+tokenizer = tiktoken.get_encoding("gpt2")
+
+# weirdword = tokenizer.encode("akwirw ier")
+# print(weirdword)
+# print(tokenizer.decode([220]))
+
+with open ("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text  = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+import torch 
+from torch.utils.data import Dataset, Dataloader
+class  GPTDatasetV1 (Dataset):
+    def __init__ (self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+        for i in range(0, len(token_ids) - max_length , stride):
+            input_chunk = token_ids[i:i+max_length]
+            target_chunk = token_ids[i+1:i+max_length+1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def  __len__ (self):
+        return len(self.input_ids)
+    
+    def __getitem__ (self,idx):
+        return self.input_ids[idx], self.target_ids[idx]
+max_length = 4
+def create_dataloader_v1 (txt, max_length=256, batch_Size=4, stride=128, shuffle=True, drop_last=True, num_workers = 0 ):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1 (txt, tokenizer, max_length, stride)
+    dataloader = Dataloader(
+        dataset,
+        batch_size= batch_Size,
+        shuffle = shuffle,
+        drop_last = drop_last,
+        num_workers = num_workers
+    )
+    #data = tokenizer.encode(raw_text)
+    return dataloader
+
+dataloader = create_dataloader_v1 (raw_text, max_length=max_length, batch_Size = 8, stride = max_length, shuffle = False )
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+out_dim = 256
+token_embedding_layer = torch.nn.embedding(max_length, out_dim)
+token_embedding = token_embedding_layer(inputs)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.embedding(context_length, out_dim)
+pos_embedding = pos_embedding_layer(torch.arange(context_length)) #torch arrange is [0,1,...,context_len-1]
+
+input_embeddings = token_embedding + pos_embedding
